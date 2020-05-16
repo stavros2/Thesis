52fa3a8371c788d18419d310f94d87868251d22d
Stavros Stavrou <45168559+stavros2@users.noreply.github.com>
2020-05-17 00:55:48 +0300

    fogUser Update
    Methods used for node selection during SLA

--- src/fogUser.py
+++ src/fogUser.py
@@ -1,11 +1,13 @@
-import random;
-import numpy as np;
+import random
+import numpy as np
+import constants
 
 class fogUser:
     def __init__(self, k):
         # the fogUser object is initialized with a random task, represented by a tuple as follows (application bits, application intense parameter, application CPU cycles)
         self.ongoingTask = (0, 0, 0);
         self.probVector = np.ones(k);
+        self.noNodes = k;
         self.eu = random.randint(0, 100000000000000)
         self.initTask();
         self.initProbs(k);
@@ -21,4 +23,26 @@ class fogUser:
         self.probVector = np.ones(k);
         self.probVector /= k;
         
+    def hasDecided(self):
+        return any(self.probVector > 0.95);
+    
+    def giveSelection(self):
+        return self.probVector.argmax();
+    
+    def chooseNode(self):
+        temp = random.random();
+        i = 0;
         
+        while temp > self.probVector[i]:
+            temp -= self.probVector[i];
+            i += 1;
+            
+        return i
+    
+    
+    def updateProbs(self, curNode, reward):
+        for i in range(self.noNodes):
+            if i == curNode:
+                self.probVector[i] = self.probVector[i] + constants.beta * reward * (1 - self.probVector[i]);
+            else:
+                self.probVector[i] = self.probVector[i] - constants.beta * reward * self.probVector[i];
